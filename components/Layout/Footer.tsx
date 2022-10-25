95a7a10278aeb2b24c0ea52c4dd457bf3a9cb8b4
anoboy21 <109586478+anoboy21@users.noreply.github.com>
2022-10-25 17:58:03 +0800

    Update Footer.tsx

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className="flex justify-end items-center h-16 mt-8 sm:mt-12 sm:mb-20 px-6 bg-stone-800">
       <div className="flex items-center gap-3">
         <span className="text-xl">Contact Me :</span>
-        <Link href="https://github.com/NhiThanhLuong/next-movie">
+        <Link href="https://github.com/">
           <a
             target="_blank"
             title="Github"
@@ -15,7 +15,7 @@ const Footer = () => {
             <FaGithub size={24} />
           </a>
         </Link>
-        <Link href="https://www.facebook.com/luongthanh.nhi/">
+        <Link href="">
           <a
             target="_blank"
             title="Facebook"
